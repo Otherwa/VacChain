e0bb43bd9e26ac5654cdcc61e7020f6084b612bc
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2024-04-08 20:53:14 +0530

    /server done only admin side block creation technique /

--- server/modules/servernode.cjs
+++ server/modules/servernode.cjs
@@ -17,27 +17,24 @@ var STATUS = "inactive"
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/addTransaction', (req, res) => {
-    const transactionData = req.body;
-    blockchain.addTransaction(transactionData);
-    res.status(200).send('Transaction added to pending transactions.');
-});
-
+// ? add certifctes
 app.post('/addCertificateData', (req, res) => {
     const certificateData = req.body;
     blockchain.addCertificate(certificateData);
     res.status(200).send('Certificate data added to pending transactions.');
 });
 
-app.get('/', (req, res) => {
-    res.render('dash', { status: STATUS })
-})
 
-app.post('/addBlock', (req, res) => {
+// ? mine a block
+app.post('/mineBlock', (req, res) => {
     const blockData = req.body;
     blockchain.addBlock(blockData);
     res.status(200).send('Block added to the blockchain.');
 });
+
+app.get('/', (req, res) => {
+    res.render('dash', { status: STATUS })
+})
 
 app.get('/getBlocks', (req, res) => {
     const blocks = blockchain.getBlockchainData();
